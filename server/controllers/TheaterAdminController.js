b38fb0b8642be8cb7ac9f7a832cca7eeb4fc6091
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-16 02:40:16 +0530

    working on server...

--- server/controllers/TheaterAdminController.js
+++ server/controllers/TheaterAdminController.js
@@ -1,6 +1,54 @@
-async function loginTheaterAdmin(req, res) {
-    const { email, password } = req.body;
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+async function loginTheaterAdmin(req, res) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const existingTheaterAdmin = await TheaterAdmin.findOne({ email });
+    if (!existingTheaterAdmin) {
+      return res.status(404).json({ message: "Theater Admin not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingTheaterAdmin.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingTheaterAdmin._id,
+        email: existingTheaterAdmin.email,
+        name: existingTheaterAdmin.name,
+        role: existingTheaterAdmin.role
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: true,
+      sameSite: "None"
+    });
+
+    existingTheaterAdmin.password = undefined;
+
+    res.status(200).json({
+      message: "Theater admin loggin successful",
+      TheaterAdmin: existingTheaterAdmin
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error while login" });
+  }
 }
 
-export {loginTheaterAdmin}
+export { loginTheaterAdmin };
