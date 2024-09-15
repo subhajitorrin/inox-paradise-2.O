b21571a9b98786003e9b5400cd1fba509b0cc2fc
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-16 00:58:47 +0530

    working on server...

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import MasterAdminRotuer from "./routers/MasterAdminRouters.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/masteradmin", MasterAdminRotuer);
 
 mongoose
   .connect(MONGOURL)
