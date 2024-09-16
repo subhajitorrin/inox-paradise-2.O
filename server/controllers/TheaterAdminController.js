5ca2ef176a0a471a1c98a95ab1a666fce60e839b
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-16 09:05:09 +0530

    working on server...

--- server/controllers/TheaterAdminController.js
+++ server/controllers/TheaterAdminController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import TheaterAdminModel from "../models/TheaterAdmin.js";
 import MasterAdminModel from "../models/MasterAdmin.js";
+import mailSender from "../utils/SendMail.js";
 
 async function loginTheaterAdmin(req, res) {
   const { email, password } = req.body;
@@ -58,4 +59,55 @@ async function loginTheaterAdmin(req, res) {
   }
 }
 
-export { loginTheaterAdmin };
+async function loginTheaterAdminWithOtp(req, res) {
+  const { email } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    const isMasterAdmin = MasterAdminModel.findOne({ email });
+    if (isMasterAdmin) {
+      return res.status(401).json({ message: "Can't login as master admin!" });
+    }
+
+    const existingTheaterAdmin = await TheaterAdminModel.findOne({ email });
+    if (!existingTheaterAdmin) {
+      return res.status(404).json({ message: "Theater Admin not found" });
+    }
+
+    const otp = generateOtp(5);
+
+    const newOtp = await otpModel.create({
+      email: existingTheaterAdmin.email,
+      otp: otp
+    });
+
+    await newOtp.save();
+
+    const obj = {
+      email: existingTheaterAdmin.email,
+      title: "OTP for INOX PARADISE",
+      body: `Your OTP is ${otp}`
+    };
+
+    await mailSender(obj);
+
+    return res
+      .status(200)
+      .json({ message: "Otp send successfully", otpid: newOtp._id });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Error while login theater admin with otp" });
+  }
+}
+
+function generateOtp(n) {
+  const firstDigit = Math.floor(Math.random() * 9) + 1;
+  const remainingDigits = Math.floor(Math.random() * Math.pow(10, n - 1));
+  const otp = firstDigit * Math.pow(10, n - 1) + remainingDigits;
+  return otp;
+}
+
+export { loginTheaterAdmin, loginTheaterAdminWithOtp };
