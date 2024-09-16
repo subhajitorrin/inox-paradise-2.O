c826c940441b7a54a473d3658a63c9683617de74
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-16 21:56:10 +0530

    working on server...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Admin from "./components/Admin";
+import MasterAdmin from "./components/MasterAdmin";
 
 function App() {
   return (
     <div className="text-red-600">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/masteradmin" element={<MasterAdmin />} />
       </Routes>
     </div>
   );
