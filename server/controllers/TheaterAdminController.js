bd62e17f0746831da3926d7defff7deb3022ec5b
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-18 01:20:19 +0530

    working on client...

--- server/controllers/TheaterAdminController.js
+++ server/controllers/TheaterAdminController.js
@@ -6,61 +6,6 @@ import otpModel from "../models/OTP.js";
 import mailSender from "../utils/SendMail.js";
 import generateOtp from "../utils/generateOtp.js";
 
-async function loginTheaterAdmin(req, res) {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-
-  try {
-    const isMasterAdmin = await MasterAdminModel.findOne({ email });
-    if (isMasterAdmin) {
-      return res.status(401).json({ message: "Can't login as master admin!" });
-    }
-
-    const existingTheaterAdmin = await TheaterAdminModel.findOne({ email });
-    if (!existingTheaterAdmin) {
-      return res.status(404).json({ message: "Theater Admin not found" });
-    }
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      existingTheaterAdmin.password
-    );
-    if (!isPasswordCorrect) {
-      return res.status(401).json({ message: "Wrong password" });
-    }
-
-    const token = jwt.sign(
-      {
-        id: existingTheaterAdmin._id,
-        email: existingTheaterAdmin.email,
-        name: existingTheaterAdmin.name,
-        role: existingTheaterAdmin.role
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: "30d" }
-    );
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      secure: true,
-      sameSite: "None"
-    });
-
-    existingTheaterAdmin.password = undefined;
-
-    res.status(200).json({
-      message: "Theater admin login successful",
-      TheaterAdmin: existingTheaterAdmin
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Error while login" });
-  }
-}
-
 async function loginTheaterAdminWithOtp(req, res) {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -174,9 +119,29 @@ async function logout(req, res) {
   }
 }
 
+async function getTheaterAdmin(req, res) {
+  const { role } = req;
+  if (role !== "theateradmin") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const { id } = req;
+  try {
+    const theaterAdmin = await TheaterAdminModel.findById(id);
+    if (!theaterAdmin) {
+      return res.status(404).json({ message: "Theater Admin not found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Theater Admin found", theaterAdmin: theaterAdmin });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Error fetchinig admin" });
+  }
+}
+
 export {
-  loginTheaterAdmin,
   loginTheaterAdminWithOtp,
   verifyOtpForTheaterAdmin,
-  logout
+  logout,
+  getTheaterAdmin
 };
--- server/routers/TheaterAdmin.js
+++ server/routers/TheaterAdmin.js
@@ -1,12 +1,14 @@
 import express from "express";
+import AuthToken from "../middleware/AuthToken.js"
 import {
-  loginTheaterAdmin,
   loginTheaterAdminWithOtp,
   logout,
-  verifyOtpForTheaterAdmin
+  verifyOtpForTheaterAdmin,
+  getTheaterAdmin
 } from "../controllers/TheaterAdminController.js";
 const TheaterRouter = express.Router();
 TheaterRouter.post("/theateradmin/send-otp", loginTheaterAdminWithOtp);
 TheaterRouter.post("/theateradmin/verify-otp", verifyOtpForTheaterAdmin);
+TheaterRouter.get("/theateradmin/get-admin",AuthToken,getTheaterAdmin)
 TheaterRouter.post("/logout", logout);
 export default TheaterRouter;
