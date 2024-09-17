9f62f18c2bec911c7e30181c818ec6b4eb0ebbd7
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-17 17:08:37 +0530

    working on client...

--- client/src/components/MasterAdmin/AddTheater.jsx
+++ client/src/components/MasterAdmin/AddTheater.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function AddTheater() {
   return (
     <div className="h-full flex">
-      <div className="h-full w-[30%] border-r p-[10px]">
-        <form className="gap-[20px] py-[3%] px-[10px] rounded-[10px] bg-[#353333] flex justify-center items-center flex-col">
+      <div className="h-full border-r p-[20px] ">
+        <form className="w-[350px] gap-[20px] py-[1.5rem] p-[20px] rounded-[10px] bg-[#353333] flex justify-center items-center flex-col">
           <h2 className="text-[20px] font-[500] text-center mb-[.5rem]">
             Add new theater
           </h2>
-          <div className="flex gap-[10px] items-center justify-between w-full ">
-            <label htmlFor="theatername" className="text-[14px] font-[500]">
+          <div className="flex gap-[10px] text-[14px] items-center justify-between w-full ">
+            <label htmlFor="theatername" className=" font-[500]">
               Theater name
             </label>
             <input
@@ -17,11 +17,24 @@ function AddTheater() {
               placeholder="Theater name"
               id="theatername"
               required
-              className="bg-[#2a2828] py-[5px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
+              className="bg-[#2a2828] w-[200px] py-[5px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
             />
           </div>
-          <div className="flex gap-[10px] items-center justify-between w-full">
-            <label htmlFor="adminemail" className="text-[14px] font-[500]">
+          <div className="flex text-[14px] gap-[10px] items-center justify-between w-full">
+            <label htmlFor="theateraddress" className=" font-[500]">
+              Address
+            </label>
+            <textarea
+              type="text"
+              placeholder="Theater address"
+              id="theateraddress"
+              required
+              rows={3}
+              className="bg-[#2a2828] resize-none w-[200px] py-[5px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
+            />
+          </div>
+          <div className="flex gap-[10px] text-[14px] items-center justify-between w-full">
+            <label htmlFor="adminemail" className=" font-[500]">
               Admin email
             </label>
             <input
@@ -29,7 +42,20 @@ function AddTheater() {
               placeholder="Admin email"
               id="adminemail"
               required
-              className="bg-[#2a2828] py-[5px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
+              className="bg-[#2a2828] py-[5px] w-[200px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
+            />
+          </div>
+
+          <div className="flex text-[14px] gap-[10px] items-center justify-between w-full">
+            <label htmlFor="adminpassword" className="font-[500]">
+              Password
+            </label>
+            <input
+              type="text"
+              placeholder="Admin password"
+              id="adminpassword"
+              required
+              className="bg-[#2a2828] py-[5px] w-[200px] px-[20px] outline-none rounded-[5px] border-[#ffffff44]"
             />
           </div>
 
@@ -38,7 +64,7 @@ function AddTheater() {
           </button>
         </form>
       </div>
-      <div className="h-full w-[70%]"></div>
+      <div className="h-full"></div>
     </div>
   );
 }
