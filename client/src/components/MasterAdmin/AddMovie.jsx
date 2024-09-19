2464abe0397ca96cb2cf71170cb45fe213392ae2
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-19 20:49:37 +0530

    working on client...

--- client/src/components/MasterAdmin/AddMovie.jsx
+++ client/src/components/MasterAdmin/AddMovie.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
+const genres = [
+  "Action",
+  "Comedy",
+  "Drama",
+  "Horror",
+  "Romance",
+  "Thriller",
+  "Western",
+  "Documentary"
+];
+
 function AddMovie() {
   return (
     <div className="h-full w-full p-[1rem]">
       <div className="">
-        <div className="flex gap-[10px] items-center">
+        {/* title */}
+        <div className="flex gap-[10px] items-center mb-[1rem]">
           <label htmlFor="movietitle" className="font-[500]">
-            Movie title
+            Movie Title
           </label>
           <input
             type="text"
@@ -15,8 +27,25 @@ function AddMovie() {
             className="bg-[#353333] outline-none rounded-[5px] px-[20px] py-[5px] w-[400px] text-[14px]"
           />
         </div>
+
+        {/* genre */}
+        <div className="flex gap-[10px] items-center mb-[1rem]">
+          <label htmlFor="genre" className="font-[500]">
+            Genre
+          </label>
+          <select
+            id="genre"
+            className="bg-[#353333] outline-none rounded-[5px] px-[20px] py-[5px] w-[400px] text-[14px]"
+          >
+            <option value="">Select genre</option>
+            {genres.map((genre) => (
+              <option key={genre} value={genre.toLowerCase()}>
+                {genre}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div className=""></div>
     </div>
   );
 }
