5dfdce0bd7c38fe694bd6c43e6e270ef327e9c20
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-19 20:37:49 +0530

    working on client...

--- client/src/components/MasterAdmin/AddMovie.jsx
+++ client/src/components/MasterAdmin/AddMovie.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function AddMovie() {
   return (
-    <div className="h-full w-full"></div>
-  )
+    <div className="h-full w-full p-[1rem]">
+      <div className="">
+        <div className="flex gap-[10px] items-center">
+          <label htmlFor="movietitle" className="font-[500]">
+            Movie title
+          </label>
+          <input
+            type="text"
+            placeholder="Enter movie title"
+            id="movietitle"
+            className="bg-[#353333] outline-none rounded-[5px] px-[20px] py-[5px] w-[400px] text-[14px]"
+          />
+        </div>
+      </div>
+      <div className=""></div>
+    </div>
+  );
 }
 
-export default AddMovie
+export default AddMovie;
