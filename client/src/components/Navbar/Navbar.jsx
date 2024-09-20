770f16d8664a9b6505d3da387e0952daf61418f9
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-20 21:06:25 +0530

    navbar ui

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { IoMdSearch } from "react-icons/io";
+import { IoChevronDownSharp } from "react-icons/io5";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoTicketOutline } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div className="w-full h-[100px] bg-[white] flex justify-between items-center">
-      <h2 className="text-black font-bold text-[22px]">INOX PARADISE</h2>
-      <div className="relative w-[400px] flex items-center">
-        <input
-          type="text"
-          className="w-[100%] py-[5px] rounded-[10px] px-[20px] text-[14px] text-black border border-[#00000031] outline-none"
-          placeholder="Search your favourite movie"
-        />
-        <IoMdSearch className="text-[22px] absolute right-[5px] text-black" />
+    <div className="w-full py-[20px] bg-[white] flex justify-between items-center px-[10%]">
+      <div className="flex gap-[2rem] items-center">
+        <h2 className="text-black font-bold text-[22px] flex items-center gap-[2px]">
+          <span>INOX</span>
+          <IoTicketOutline className="text-[30px]" />
+          <span>PARADISE</span>
+        </h2>
+
+        <div className="relative w-[35vw] flex items-center">
+          <input
+            type="text"
+            className="w-[100%] py-[5px] rounded-[10px] px-[20px] text-[14px] text-black border border-[#00000031] outline-none"
+            placeholder="Search your favourite movie"
+          />
+          <IoMdSearch className="text-[22px] absolute right-[5px] text-black" />
+        </div>
+      </div>
+
+      <div className="flex gap-[2rem] items-center">
+        <div className="flex text-black items-center cursor-pointer gap-[2px]">
+          <IoChevronDownSharp className="text-[14px]" />
+          <span className="text-[14px] font-[500]">Kolkata</span>
+        </div>
+
+        <div className="">
+          <button className="font-[500] bg-[#F84464] text-white text-[14px] py-[2px] px-[1.5rem] rounded-[7px]">
+            Login
+          </button>
+        </div>
+
+        <AiOutlineMenu className="text-[22px] cursor-pointer" />
       </div>
     </div>
   );
