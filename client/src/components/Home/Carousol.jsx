909f82572db27a361834a55bee72ca625e5a3b5e
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-20 21:11:41 +0530

    carousol

---
+++ client/src/components/Home/Carousol.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Carousol() {
+  return <div className="w-full h-[300px] bg-[#eee9e9]"></div>;
+}
+
+export default Carousol;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Carousol from "../components/Home/Carousol";
 
 function Home() {
   return (
     <div className="bg-[#F5F5F5] min-h-screen w-full">
       <Navbar />
+      <Carousol />
     </div>
   );
 }
