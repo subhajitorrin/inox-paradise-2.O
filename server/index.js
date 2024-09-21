d36959a72e96849321cd1cde5e92295a8c8711b3
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-21 15:52:02 +0530

    local test

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://192.168.1.196:5173"],
     credentials: true
   })
 );
