0aec136a25e3bc4f58aa9a2fee0dd179f1a0a13a
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-28 13:06:36 +0530

    update Navbar.jsx

---
+++ client/src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="bg-[#00000049] z-[100] fixed w-full h-screen transition-all ease-linear duration-200 backdrop-filter backdrop-blur-[5px]"></div>
+  );
+}
+
+export default Login;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -13,6 +13,7 @@ import SearchCard from "./SearchCard.jsx";
 import { SyncLoader } from "react-spinners";
 import { CgSearch } from "react-icons/cg";
 import { LiaSadCrySolid } from "react-icons/lia";
+import Login from "../Login/Login.jsx";
 
 function Navbar() {
   const { isMobile } = useMobile();
@@ -99,80 +100,83 @@ function Navbar() {
   }, [searchText]);
 
   return (
-    <div className="relative w-full py-[20px] bg-[white] flex justify-between items-center px-[10%]">
-      {/* search container */}
-      {searchText !== "" && (
-        <div className="top-[100%] overflow-y-auto left-[0] absolute z-[100] h-[90vh] w-full bg-white px-[10%]">
-          {searchLoading ? (
-            <div className=" h-[200px] flex flex-col gap-[20px] items-center justify-center">
-              <SyncLoader color="#b4b4b4" size={10} />
-              <p className="text-[1.1rem] font-[500] flex items-center gap-[3px]">
-                Searching Movie{" "}
-                <CgSearch className="text-[1.1rem] relative top-[2px]" />
-              </p>
-            </div>
-          ) : searchedList && searchedList.length === 0 ? (
-            <div className=" h-[200px] flex flex-col gap-[20px] items-center justify-center">
-              <p className="text-[1.1rem] text-[#b40303] font-[500] flex items-center gap-[3px]">
-                Nothing found!
-              </p>
-            </div>
-          ) : (
-            searchedList.map((item, index) => {
-              return <SearchCard item={item} key={index} />;
-            })
-          )}
-        </div>
-      )}
+    <>
+      {/* <Login /> */}
+      <div className="relative w-full py-[20px] bg-[white] flex justify-between items-center px-[10%]">
+        {/* search container */}
+        {searchText !== "" && (
+          <div className="top-[100%] overflow-y-auto left-[0] absolute z-[100] h-[90vh] w-full bg-white px-[10%]">
+            {searchLoading ? (
+              <div className=" h-[200px] flex flex-col gap-[20px] items-center justify-center">
+                <SyncLoader color="#b4b4b4" size={10} />
+                <p className="text-[1.1rem] font-[500] flex items-center gap-[3px]">
+                  Searching Movie{" "}
+                  <CgSearch className="text-[1.1rem] relative top-[2px]" />
+                </p>
+              </div>
+            ) : searchedList && searchedList.length === 0 ? (
+              <div className=" h-[200px] flex flex-col gap-[20px] items-center justify-center">
+                <p className="text-[1.1rem] text-[#b40303] font-[500] flex items-center gap-[3px]">
+                  Nothing found!
+                </p>
+              </div>
+            ) : (
+              searchedList.map((item, index) => {
+                return <SearchCard item={item} key={index} />;
+              })
+            )}
+          </div>
+        )}
 
-      <div className="flex gap-[2rem] items-center">
-        <h2
-          onClick={() => {
-            navigate("/");
-          }}
-          className="text-black cursor-pointer font-bold text-[22px] flex items-center gap-[2px]"
-        >
-          <span>INOX</span>
-          <IoTicketOutline className="text-[30px]" />
-          <span>PARADISE</span>
-        </h2>
-
-        <div className="relative w-[35vw] flex items-center">
-          <input
-            type="text"
-            onChange={(e) => {
-              setSearchLoading(true);
-              setSearchText(e.target.value);
+        <div className="flex gap-[2rem] items-center">
+          <h2
+            onClick={() => {
+              navigate("/");
             }}
-            value={searchText}
-            className="w-[100%] pr-[60px] py-[5px] rounded-[10px] px-[20px] text-[14px] text-black border border-[#00000031] outline-none"
-            placeholder="Search your favourite movie"
-          />
-          {searchText !== "" && (
-            <RxCross2
-              onClick={() => setSearchText("")}
-              className="text-[18px] cursor-pointer absolute right-[35px] text-black"
+            className="text-black cursor-pointer font-bold text-[22px] flex items-center gap-[2px]"
+          >
+            <span>INOX</span>
+            <IoTicketOutline className="text-[30px]" />
+            <span>PARADISE</span>
+          </h2>
+
+          <div className="relative w-[35vw] flex items-center">
+            <input
+              type="text"
+              onChange={(e) => {
+                setSearchLoading(true);
+                setSearchText(e.target.value);
+              }}
+              value={searchText}
+              className="w-[100%] pr-[60px] py-[5px] rounded-[10px] px-[20px] text-[14px] text-black border border-[#00000031] outline-none"
+              placeholder="Search your favourite movie"
             />
-          )}
-          <IoMdSearch className="text-[22px] absolute right-[5px] text-black" />
+            {searchText !== "" && (
+              <RxCross2
+                onClick={() => setSearchText("")}
+                className="text-[18px] cursor-pointer absolute right-[35px] text-black"
+              />
+            )}
+            <IoMdSearch className="text-[22px] absolute right-[5px] text-black" />
+          </div>
+        </div>
+
+        <div className="flex gap-[2rem] items-center">
+          <div className="flex text-black items-center cursor-pointer gap-[2px]">
+            <IoChevronDownSharp className="text-[14px]" />
+            <span className="text-[14px] font-[500]">Kolkata</span>
+          </div>
+
+          <div className="">
+            <button className="font-[500] bg-[#F84464] text-white text-[14px] py-[2px] px-[1.5rem] rounded-[7px]">
+              Login
+            </button>
+          </div>
+
+          <AiOutlineMenu className="text-[22px] cursor-pointer" />
         </div>
       </div>
-
-      <div className="flex gap-[2rem] items-center">
-        <div className="flex text-black items-center cursor-pointer gap-[2px]">
-          <IoChevronDownSharp className="text-[14px]" />
-          <span className="text-[14px] font-[500]">Kolkata</span>
-        </div>
-
-        <div className="">
-          <button className="font-[500] bg-[#F84464] text-white text-[14px] py-[2px] px-[1.5rem] rounded-[7px]">
-            Login
-          </button>
-        </div>
-
-        <AiOutlineMenu className="text-[22px] cursor-pointer" />
-      </div>
-    </div>
+    </>
   );
 }
 
