e1dd4f1c0813cb7395fc4df4408daff0fee06f39
subhajitorrin <subhajitghosh343@gmail.com>
2024-09-28 14:11:01 +0530

    update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -123,7 +123,7 @@ function Login() {
             Don&apos;t have an account?{" "}
             <div
               onClick={() => setIsLogin(false)}
-              className="font-medium text-primary hover:underline"
+              className="font-medium text-primary hover:underline cursor-pointer"
             >
               Register here
             </div>
